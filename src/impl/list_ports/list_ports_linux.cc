ad1ba27636057bc6619e9feec2a8afe75e33732a
NJ Ramadhan <nj.ramadhan.mechatronics@gmail.com>
2025-10-02 09:10:31 +0700

    Make code fit for ROS2

--- src/impl/list_ports/list_ports_linux.cc
+++ src/impl/list_ports/list_ports_linux.cc
@@ -55,15 +55,17 @@ glob(const vector<string>& patterns)
     glob_t glob_results;
 
     int glob_retval = glob(patterns[0].c_str(), 0, NULL, &glob_results);
+    (void)glob_retval; // Suppress unused variable warning
 
     vector<string>::const_iterator iter = patterns.begin();
 
     while(++iter != patterns.end())
     {
         glob_retval = glob(iter->c_str(), GLOB_APPEND, NULL, &glob_results);
+        (void)glob_retval; // Suppress unused variable warning
     }
 
-    for(int path_index = 0; path_index < glob_results.gl_pathc; path_index++)
+    for(size_t path_index = 0; path_index < glob_results.gl_pathc; path_index++)
     {
         paths_found.push_back(glob_results.gl_pathv[path_index]);
     }
@@ -243,7 +245,7 @@ format(const char* format, ...)
         {
             done = true;
         }
-        else if( return_value >= buffer_size_bytes )
+        else if( static_cast<size_t>(return_value) >= buffer_size_bytes )
         {
             // Realloc and try again.
 
